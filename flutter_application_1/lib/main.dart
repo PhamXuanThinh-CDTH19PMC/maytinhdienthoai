5df63d50f0e81b6748a5042ee204ecb081dceb9c
Toan1803 <vantoan25012001@gmail.com>
2021-10-30 21:02:48 +0700

    thay doi

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -7,13 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-  
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Máy tính trên điện thoại'),
@@ -32,24 +30,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-  
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-       
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-         
-          ],
+          children: <Widget>[Toan],
         ),
       ),
-  
     );
   }
 }
